27bbd4ef1776043ad6dd2dbad37950499dc45cc0
fabiano <fabianofelix3@gmail.com>
2018-11-18 16:03:01 -0200

    First class: Position

--- src/application/program.java
+++ src/application/program.java
@@ -1,9 +1,14 @@
 package application;
 
+import bordergame.Position;
+
 public class program {
 
 	public static void main(String[] args) {
-		System.err.println("hello word!");
+		
+		Position pos = new Position(3, 5);
+		
+		System.out.println(pos);
 
 	}
 
