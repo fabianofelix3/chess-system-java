09d33c0f0cd2e9e27c7f350464dcbc133a0f24c2
fabiano <fabianofelix3@gmail.com>
2018-11-19 18:31:57 -0200

    Placing pieces on the board

--- src/chess/ChessMacth.java
+++ src/chess/ChessMacth.java
@@ -2,6 +2,8 @@ package chess;
 
 import bordergame.Board;
 import bordergame.Position;
+import chess.pieces.King;
+import chess.pieces.Rook;
 
 public class ChessMacth {
 
@@ -9,7 +11,7 @@ public class ChessMacth {
 
 	public ChessMacth() {
 		board = new Board(8, 8);
-
+		initialSetup();
 	}
 
 	public ChessPiece[][] getPieces() {
@@ -20,5 +22,12 @@ public class ChessMacth {
 			}
 		}
 		return mat;
+	}
+
+	private void initialSetup() {
+		board.placePiece(new Rook(board, Color.WHITE), new Position(2, 1));
+		board.placePiece(new King(board, Color.BLACK), new Position(0, 4));
+		board.placePiece(new King(board, Color.WHITE), new Position(7, 4));
+		
 	}
 }
